70a853be059a37118dd95ba2c58b743a03a073af
ppprakharr <prakhar.srivastava999@gmail.com>
2024-11-23 18:06:48 +0530

    Initial devise setup

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,6 +77,9 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  # Devise setup
+  # config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
+
   # Only use :id for inspections in production.
   config.active_record.attributes_for_inspect = [ :id ]
 
