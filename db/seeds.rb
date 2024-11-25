9b101e74186afea0c83aa4924ae2f05b2172a27d
ppprakharr <prakhar.srivastava999@gmail.com>
2024-11-25 18:33:52 +0530

    property model faker added

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,18 +1,11 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
-
-properties = Property.create!(name: 'Sample property',
-headline: 'headline txt',
-description: 'descrifevlkrejvke',
-address_1: 'checl1',
-address_2: 'check3',
-city: 'test',
-state: 'abc',
-country: 'sjd')
+20.times do
+  Property.create!({
+  name: Faker::Lorem.unique.sentence(word_count: 2),
+  headline: Faker::Lorem.unique.paragraph(sentence_count: 10),
+  description: Faker::Lorem.unique.sentence(word_count: 6),
+  address_1: Faker::Address.street_address,
+  address_2: Faker::Address.street_name,
+  city: Faker::Address.city,
+  state: Faker::Address.state,
+  country: Faker::Address.country })
+end
