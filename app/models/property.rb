08f94bfde7d3eae985cded5fd8c256eab4331d11
ppprakharr <prakhar.srivastava999@gmail.com>
2024-11-26 22:25:34 +0530

    added review table and liunked to property and user

--- app/models/property.rb
+++ app/models/property.rb
@@ -11,4 +11,6 @@ class Property < ApplicationRecord
   monetize :price_cents, allow_nil: true
 
   has_many_attached :images
+
+  has_many :reviews, dependent: :destroy
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,8 @@
+user = User.create!({
+  email: 'tester@gmail.com',
+  password: '123456'
+})
+
 4.times do |i|
   property=Property.create!({
   name: Faker::Lorem.unique.sentence(word_count: 2),
@@ -13,4 +18,18 @@
   property.images.attach(io: File.open("db/images/property_#{i+1}.png"), filename: property.name)
   property.images.attach(io: File.open("db/images/property_#{i+5}.png"), filename: property.name)
   property.images.attach(io: File.open("db/images/property_#{i+9}.png"), filename: property.name)
+
+  ((5..10).to_a.sample).times do
+    Review.create!({
+      content: Faker::Lorem.unique.paragraph(sentence_count: 10),
+      cleanliness_rating: (1..5).to_a.sample,
+      accuracy_rating: (1..5).to_a.sample,
+      checkin_rating: (1..5).to_a.sample,
+      communication_rating: (1..5).to_a.sample,
+      location_rating: (1..5).to_a.sample,
+      value_rating: (1..5).to_a.sample,
+      property: property,
+      user: user
+    })
+  end
 end
