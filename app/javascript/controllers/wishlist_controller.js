15df9a829f930592b98ff391bfe9ba902079cfb4
ppprakharr <prakhar.srivastava999@gmail.com>
2024-11-28 00:11:29 +0530

    added wishlist api and linked it with db and fe

--- app/javascript/controllers/wishlist_controller.js
+++ app/javascript/controllers/wishlist_controller.js
@@ -13,16 +13,71 @@ export default class extends Controller {
         }
         
         if(this.element.dataset.status=="false"){
-        this.element.classList.remove("fill-none")
-        this.element.classList.add("fill-primary")
-        this.element.dataset.status="true"
+            const userId = this.element.dataset.userId;
+            const propertyId = this.element.dataset.propertyId;
+            this.addPropertyToWishlist(propertyId, userId)
+
+        
         
         }
         else{
+            const wishlistId = this.element.dataset.wishlistId
+            this.removePropertyFromWishlist(wishlistId)
+        }
+    }
+
+        addPropertyToWishlist(propertyId, userId){
+            const params = {
+                property_id: propertyId,
+                user_id: userId,
+            };
+            const options={
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(params)
+            };
+
+        fetch('/api/wishlists', options)
+        .then(response =>{
+            if (!response.ok){
+                console.log(response.status)
+            }
+            return response.json()
+        })
+        .then(data=>{
+            this.element.dataset.wishlistId = data.id
+            this.element.classList.remove("fill-none")
+            this.element.classList.add("fill-primary")
+            this.element.dataset.status="true"
+            console.log(data)
+        }).catch(e=>{
+            console.log(e)
+        });
+        
+}
+
+removePropertyFromWishlist(wishlistId){
+    const options={
+        method: 'DELETE',
+        headers:{
+        'Content-Type': 'application/json'
+    }};
+    fetch('/api/wishlists/'+wishlistId,options)
+    .then(response=>
+   
+        {
             this.element.classList.remove("fill-primary")
             this.element.classList.add("fill-none")
             this.element.dataset.status="false"
-        }
-}
-  }
+            this.element.dataset
+           
+
+        }).catch(e=>{
+            console.log(e)
+        });
+
+    }
+    }
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root "home#index"
+namespace :api do
+  resources :wishlists, only: [ :create, :destroy ]
+end
 
   # get "up" => "rails/health#show", as: :rails_health_check
 end
