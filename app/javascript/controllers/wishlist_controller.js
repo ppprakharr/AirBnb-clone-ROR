7df42b8043ebd8800c691537e795c690fb7bfa6a
ppprakharr <prakhar.srivastava999@gmail.com>
2024-11-27 21:02:57 +0530

    minor fixes in wishlist fe

--- app/javascript/controllers/wishlist_controller.js
+++ app/javascript/controllers/wishlist_controller.js
@@ -4,16 +4,25 @@ export default class extends Controller {
 
 
     updateWishlistStatus(){
-    if(this.element.dataset.status=="false"){
-    this.element.classList.remove("fill-none")
-    this.element.classList.add("fill-primary")
-    this.element.dataset.status="true"
-    }
-    else{
-        this.element.classList.remove("fill-primary")
-        this.element.classList.add("fill-none")
-        this.element.dataset.status="false"
-    }
-    }
+    // get status whether the user is logged in
+    // if not logged in redirect to login page,return
+        const isUserLoggedIn = this.element.dataset.userLoggedIn;
+        if (isUserLoggedIn=="false"){
+            document.getElementById("login").click()
+            return
+        }
+        
+        if(this.element.dataset.status=="false"){
+        this.element.classList.remove("fill-none")
+        this.element.classList.add("fill-primary")
+        this.element.dataset.status="true"
+        
+        }
+        else{
+            this.element.classList.remove("fill-primary")
+            this.element.classList.add("fill-none")
+            this.element.dataset.status="false"
+        }
+}
   }
 
