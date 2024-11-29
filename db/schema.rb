363a6871549588358c88910501ce021220b3fe10
ppprakharr <prakhar.srivastava999@gmail.com>
2024-11-29 20:58:24 +0530

    UI changes in property details page-1

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2024_11_28_130716) do
+ActiveRecord::Schema[8.0].define(version: 2024_11_29_134805) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -57,6 +57,10 @@ ActiveRecord::Schema[8.0].define(version: 2024_11_28_130716) do
     t.string "price_currency"
     t.integer "reviews_count"
     t.decimal "average_final_rating"
+    t.integer "guest_count", default: 0
+    t.integer "bedroom_count", default: 0
+    t.integer "bed_count", default: 0
+    t.integer "bathroom_count", default: 0
   end
 
   create_table "reservations", force: :cascade do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -3,21 +3,31 @@ user = User.create!({
   password: '123456'
 })
 
-4.times do |i|
+6.times do |i|
   property=Property.create!({
   name: Faker::Lorem.unique.sentence(word_count: 2),
-  headline: Faker::Lorem.unique.paragraph(sentence_count: 10),
-  description: Faker::Lorem.unique.sentence(word_count: 6),
+  headline: Faker::Lorem.unique.paragraph(sentence_count: 30),
+  description: Faker::Lorem.unique.sentence(word_count: 5),
   price: Money.from_amount((50..100).to_a.sample, 'USD'),
   address_1: Faker::Address.street_address,
   address_2: Faker::Address.street_name,
   city: Faker::Address.city,
   state: Faker::Address.state,
-  country: Faker::Address.country })
+  country: Faker::Address.country,
+  guest_count: (2..14).to_a.sample,
+  bedroom_count: (1..7).to_a.sample,
+  bed_count: (1..5).to_a.sample,
+  bathroom_count: (1..3).to_a.sample
+ })
 
   property.images.attach(io: File.open("db/images/property_#{i+1}.png"), filename: property.name)
-  property.images.attach(io: File.open("db/images/property_#{i+5}.png"), filename: property.name)
-  property.images.attach(io: File.open("db/images/property_#{i+9}.png"), filename: property.name)
+  property.images.attach(io: File.open("db/images/property_7.png"), filename: property.name)
+  property.images.attach(io: File.open("db/images/property_8.png"), filename: property.name)
+  property.images.attach(io: File.open("db/images/property_9.png"), filename: property.name)
+  property.images.attach(io: File.open("db/images/property_10.png"), filename: property.name)
+  property.images.attach(io: File.open("db/images/property_11.png"), filename: property.name)
+  property.images.attach(io: File.open("db/images/property_12.png"), filename: property.name)
+
 
   ((5..10).to_a.sample).times do
     Review.create!({
