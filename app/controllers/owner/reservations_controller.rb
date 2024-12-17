f3869236a561e9f583b8c06cd5e9fc50805bd0d1
ppprakharr <prakhar.srivastava999@gmail.com>
2024-12-17 19:12:28 +0530

    Monir fix in Owner reservation

--- app/controllers/owner/reservations_controller.rb
+++ app/controllers/owner/reservations_controller.rb
@@ -2,7 +2,7 @@ module Owner
   class ReservationsController < ApplicationController
     before_action :authenticate_user!
     def index
-        @reservations = current_user.reservations
+        @reservations = current_user.properties.map(&:reservations).flatten
     end
   end
 end
