757813e7be4b5d746fdcbbad18c3a34e672e60b8
ppprakharr <prakhar.srivastava999@gmail.com>
2024-12-19 18:54:40 +0530

    minor modification in model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
 
-  has_one :profile
+  has_one :profile, dependent: :destroy
   has_many :reviews, dependent: :destroy
 
   has_many :wishlists, dependent: :destroy
